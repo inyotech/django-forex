04920eeeb33154c013f62088211c429b0d734e02
scottb <scottb@inyotech.com>
2017-04-10 08:42:47 -0700

    Stlye historic table. Begin columnizing pages.

--- forex/webapp/static/webapp/js/tables.js
+++ forex/webapp/static/webapp/js/tables.js
@@ -78,3 +78,81 @@ function show_current() {
 
     });
 }
+
+function show_historic_table() {
+
+    console.log('historic_table');
+
+    var columns = 5;
+    var column_length = Math.ceil(historic_rates.data.length/columns);
+    var columnized_data = [];
+
+    for (i=0, j=historic_rates.data.length; i<j; i+=column_length) {
+        columnized_data.push(historic_rates.data.slice(i, i+column_length));
+    }
+
+    var page_length = 10;
+    var page_data = [];
+
+    for (i=0; i<column_length; i+=page_length) {
+        var page_columns = []
+        for (j=0; j<columns; j++) {
+            page_columns.push(columnized_data[j].slice(i, i+page_length));
+        }
+        page_data.push(page_columns);
+    }
+
+    var table = d3.select('#historic-table').selectAll('table#historic-rates');
+
+    var tbodies = table.selectAll('tbody').data(page_data[0]);
+
+    tbodies.exit().remove();
+
+    tbodies = tbodies.enter().append('tbody').merge(tbodies);
+
+    tbodies.style('float', 'left');
+
+    tbodies.order();
+
+    var rows = tbodies.selectAll('tr').data(function (d) {
+        return d;
+    });
+
+    rows.exit().remove();
+
+    rows = rows.enter().append('tr').merge(rows);
+
+    rows.order();
+
+    rows.attr('class', function(d, i) {
+        if ((i % 2) == 0) {
+            return 'even';
+        } else {
+            return 'odd';
+        }
+    });
+
+    var cells = rows.selectAll('td').data(function(d) {
+        return [
+            d.rate_date,
+            d.rate_ratio
+        ];
+    });
+
+    cells.exit().remove();
+
+    cells = cells.enter().append('td').merge(cells);
+
+    cells.order();
+
+    cells.text(function(d) {
+        return d;
+    }).attr('class', function(d, i) {
+        if (i == 0) {
+            return 'date';
+        } else if(i == 1) {
+            return 'rate';
+        }
+    });
+
+}
