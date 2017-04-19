646ea378dda8b8639d242220281c0365bc7c5bcf
inyotech <scottb@inyotech.com>
2017-04-19 14:59:23 +0000

    Fix path in downloader script.

--- bin/daily_load.sh
+++ bin/daily_load.sh
@@ -4,5 +4,7 @@ base_path=$(dirname $(readlink -f "$0"))/../
 
 python_interpreter=${base_path}/venv/bin/python
 
+$python_interpreter ${base_path}/forex/manage.py loadrates
+
 $python_interpreter ${base_path}/forex/manage.py loadstories
 
